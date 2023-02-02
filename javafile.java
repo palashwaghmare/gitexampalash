c87b6b568239d44bf16bd28614c5cfc7fbaa8476
palashwaghmare <cdacjuhupalash@gmail.com>
2023-02-02 14:19:21 +0530

    commit1

---
+++ javafile.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+class javafile{
+	
+		class Node {
+        int data;
+        Node next;
+ 
+        
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+		
+		}
+		
+		
+		
+	
+	 Node head = null;
+	Node tail = null;
+	
+	
+	
+	
+	public static void main(String[] args)
+    {
+ 
+        LinkedListCreation L1 = new LinkedListCreation();
+ 
+        //L1.addNode(1);
+        //L1.addNode(2);
+        //L1.addNode(3);
+        //L1.addNode(4);
+ 
+        // Displays the nodes present in the list
+        //L1.displayNodes();
+ 
+        
+    }
+	
+	
+}
